328e4902657e4568231ef620bf55f8b2e526acb1
Tushar Joshi <tusharjoshi024@gmail.com>
2020-09-24 17:17:02 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
+# React JS App with AWS Amplify + Amplify Predictions library to use transcribe service
+This app demonstrates how to build a progressive web app with React JS and AWS Amplify and incorporating a microphone service to record speech and convert the same to text.
+It also shows the usage of bootstrap in react to make the SPA responsive.
+
+## Steps to create the project
+1. Install node and npm. Create a React JS Project or clone this one.
+
+2. Go to the root directory of project. Install AWS Amplify CLI 
+   
+    ######
+        npm install -g @aws-amplify/cli
+        amplify configure
+        
+3. Acquire AWS credentials and Access Key pair and then follow the steps on CLI -  
+
+    ######
+        Specify the AWS Region
+        ? region:  # Your preferred region
+        Specify the username of the new IAM user:
+        ? user name:  # User name for Amplify IAM user
+        Complete the user creation using the AWS console
+        implementation 'com.amazonaws:aws-java-sdk-sns'
+        Enter the access key of the newly created user:
+        ? accessKeyId:  # YOUR_ACCESS_KEY_ID
+        ? secretAccessKey:  # YOUR_SECRET_ACCESS_KEY
+        This would update/create the AWS Profile in your local machine
+        ? Profile Name:  # (default)
+        
+        Successfully set up the new user.
+
+4. Once your Amplify CLI is configured with AWS credentials, initialize the backend -
+   
+    ######
+        amplify init
+        
+5. Complete the activities on CLI to initialize the backend, and install following required libraries.  
+    ######
+        npm install aws-amplify @aws-amplify/ui-react
+        npm install aws-amplify-react
+
+6. Add these amplify dependencies - 
+    ######
+        amplify add auth // use default, add access to guest
+        amplify add predictions // select to use the transcribe, speech to text
+        amplify push
+
+7. Install the microphone and other dependencies 
+    ######
+        npm install microphone-stream
+        npm install --save bootstrap@latest
+        npm install --save font-awesome
+
+8. To make the application PWA (workable while offline) make changes in index.js-
+    ###### index.js
+        // If you want your app to work offline and load faster, you can change
+        // unregister() to register() below. Note this comes with some pitfalls.
+        // Learn more about service workers: https://bit.ly/CRA-PWA
+        serviceWorker.register(); 
+       
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -66,3 +125,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+## Reference AWS Documentation - 
+You can learn more in the [AWS Amplify Sample Predictions App documentation](https://docs.amplify.aws/lib/predictions/sample/q/platform/js#sample-react-app).
--- src/App.js
+++ src/App.js
@@ -1,26 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Amplify, {Storage, Predictions} from 'aws-amplify';
+import {AmazonAIPredictionsProvider} from '@aws-amplify/predictions';
+
+import awsconfig from './aws-exports';
+
+import mic from 'microphone-stream';
+
+
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
+
+function SpeechToText(props) {
+    const [response, setResponse] = useState("Press 'start recording' to begin your transcription. Press STOP recording once you finish speaking.")
+
+    function AudioRecorder(props) {
+        const [recording, setRecording] = useState(false);
+        const [micStream, setMicStream] = useState();
+        const [audioBuffer] = useState(
+            (function () {
+                let buffer = [];
+
+                function add(raw) {
+                    buffer = buffer.concat(...raw);
+                    return buffer;
+                }
+
+                function newBuffer() {
+                    console.log("resetting buffer");
+                    buffer = [];
+                }
+
+                return {
+                    reset: function () {
+                        newBuffer();
+                    },
+                    addData: function (raw) {
+                        return add(raw);
+                    },
+                    getData: function () {
+                        return buffer;
+                    }
+                };
+            })()
+        );
+
+        async function startRecording() {
+            console.log('start recording');
+            audioBuffer.reset();
+
+            window.navigator.mediaDevices.getUserMedia({video: false, audio: true}).then((stream) => {
+                const startMic = new mic();
+
+                startMic.setStream(stream);
+                startMic.on('data', (chunk) => {
+                    var raw = mic.toRaw(chunk);
+                    if (raw == null) {
+                        return;
+                    }
+                    audioBuffer.addData(raw);
+
+                });
+
+                setRecording(true);
+                setMicStream(startMic);
+            });
+        }
+
+        async function stopRecording() {
+            console.log('stop recording');
+            const {finishRecording} = props;
+
+            micStream.stop();
+            setMicStream(null);
+            setRecording(false);
+
+            const resultBuffer = audioBuffer.getData();
+
+            if (typeof finishRecording === "function") {
+                finishRecording(resultBuffer);
+            }
+
+        }
+
+        return (
+            <div className="audioRecorder card-body align-content-center align-items-center text-center bg-light border-top border-dark">
+                <div>
+                    {recording && <button className="btn-lg btn-primary" onClick={stopRecording}>
+                        <i className="fa fa-2x fa-microphone-slash"></i>
+                    </button>}
+                    {!recording && <button className="btn-lg btn-primary" onClick={startRecording}>
+                        <i className="fa fa-2x fa-microphone"></i>
+                    </button>}
+                </div>
+            </div>
+        );
+    }
+
+    function convertFromBuffer(bytes) {
+        setResponse('Converting text...');
+
+        Predictions.convert({
+            transcription: {
+                source: {
+                    bytes
+                },
+                // language: "en-US", // other options are "en-GB", "fr-FR", "fr-CA", "es-US"
+            },
+        }).then(({transcription: {fullText}}) => setResponse(fullText))
+            .catch(err => setResponse(JSON.stringify(err, null, 2)))
+    }
+
+    return (
+        <div className="Text card-body bg-light border-primary border-bottom border-right border-left">
+            <div>
+                <h4>Speech to text output</h4>
+                <p>{response}</p>
+                <AudioRecorder finishRecording={convertFromBuffer}/>
+
+            </div>
+        </div>
+    );
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App card">
+            <div className="card-header bg-primary font-light"><h3>Transcribe Audio</h3></div>
+            <SpeechToText/>
+            <br/>
+        </div>
+    );
 }
 
 export default App;
